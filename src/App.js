25d951ddc85fa4596caa6b7d2bb67166d3524964
manhtuong24865 <Ndmt239@gmail.com>
2023-12-26 12:40:55 +0700

    ShoesFe

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { routes } from './routes'
+import DefalutComponent from './components/DefaultComponent/DefaultComponent'
+import { isJsonString } from './utils'
+import { jwtDecode } from 'jwt-decode'
+import * as UserServices from "./services/UserServices";
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUser } from './redux/slice/UserSlice'
+import LoadingComponent from "./components/LoadingComponent/LoadingComponent"
+
+
+
+function App() {
+  const dispatch = useDispatch();
+  const [isPending, setIsPending] = useState(false)
+
+  const user = useSelector((state) => state.user)
+  useEffect(() => {
+    setIsPending(true)
+    const { storageData, decoded } = handleDecoded()
+    if (decoded?.id) {
+      handleGetDetailsUser(decoded?.id, storageData)
+    } setIsPending(false)
+  }, [])
+
+  const handleDecoded = () => {
+    let storageData = localStorage.getItem('access_token')
+    let decoded = {}
+    if (storageData && isJsonString(storageData)) {
+      storageData = JSON.parse(storageData)
+      decoded = jwtDecode(storageData)
+    }
+    return { decoded, storageData }
+
+  }
+  UserServices.axiosJWT.interceptors.request.use(async (config) => {
+    // Do something before request is sent
+    const currentTime = new Date()
+    const { decoded } = handleDecoded()
+    if (decoded?.exp < currentTime.getTime() / 1000) {
+      const data = await UserServices.refreshToken()
+      config.headers['token'] = `Bearer ${data?.access_token}`
+    }
+    return config;
+  }, (err) => {
+    return Promise.reject(err);
+  });
+
+
+  const handleGetDetailsUser = async (id, token) => {
+    const res = await UserServices.getDetailsUser(id, token)
+    dispatch(updateUser({ ...res?.data, access_token: token }))
+  }
+  return (
+    <div>
+      <LoadingComponent isPending={isPending}>
+        <Router>
+          <Routes>
+            {routes.map((route) => {
+              const Page = route.page
+              const isCheckAuth = !route.isPrivate || user.isAdmin
+              const Layout = route.isShowHeader ? DefalutComponent : Fragment
+              return (
+                <Route path={route.path} key={isCheckAuth && route.path} element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                } />
+              )
+            }
+            )}
+          </Routes>
+        </Router>
+      </LoadingComponent>
+    </div>
+  )
+}
+
+export default App
---
+++ src/routes/index.js
@@ -0,0 +1,60 @@
+import AdminPage from "../pages/AdminPage/AdminPage";
+import HomePage from "../pages/HomePage/HomePage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import OderPage from "../pages/OderPage/OderPage";
+import ProductDetailsPage from "../pages/ProductDetailsPage/ProductDetailsPage";
+import ProductsPage from "../pages/ProductsPage/ProductsPage"
+import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import SignInPage from "../pages/SignInPage/SignInPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
+import TypeProductPage from "../pages/TypeProductPage/TypeProductPage";
+export const routes = [
+    {
+        path: '/',
+        page: HomePage,
+        isShowHeader: true
+    },
+    {
+        path: '/order',
+        page: OderPage,
+        isShowHeader: true
+    },
+    {
+        path: '/product',
+        page: ProductsPage,
+        isShowHeader: true
+    },
+    {
+        path: '/product/:type',
+        page: TypeProductPage,
+        isShowHeader: true
+    },
+    {
+        path: '/sign-in',
+        page: SignInPage,
+        isShowHeader: true
+    }, {
+        path: '/sign-up',
+        page: SignUpPage,
+        isShowHeader: true
+    }, {
+        path: '/product-details/:id',
+        page: ProductDetailsPage,
+        isShowHeader: true
+    },
+    {
+        path: '/profile-user',
+        page: ProfilePage,
+        isShowHeader: true
+    },
+    {
+        path: '/system/admin',
+        page: AdminPage,
+        isShowHeader: false,
+        isPrivate: true
+    },
+    {
+        path: '*',
+        page: NotFoundPage
+    }
+]
